fc7e32e6c8e9c19252e45710cea55ab7485b3d26
Paul Martel <shmorty@gmail.com>
2022-09-18 19:37:37 -0400

    add nx lib for ui tabs component

--- apps/omni-method/src/app/app.module.ts
+++ apps/omni-method/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiModule } from '@omni-method-nx/ui';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, UiModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ libs/ui/src/lib/tabs/tabs.component.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'omni-method-nx-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+})
+export class TabsComponent implements OnInit {
+  constructor() {}
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {}
+}
