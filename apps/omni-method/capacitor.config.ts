2e74f0c84a64e6b878c3bffe7d8d2743a23dfda6
Paul Martel <shmorty@gmail.com>
2022-09-18 18:05:52 -0400

    setup nx nxtend for capacitor

---
+++ apps/omni-method/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shmorty.omni-method',
+  appName: 'omni-method',
+  webDir: '../../dist/apps/omni-method',
+  bundledWebRuntime: false,
+};
+
+export default config;
