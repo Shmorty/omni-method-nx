e82905076ade93b8c61756448c28efde42f13bd2
Paul Martel <shmorty@gmail.com>
2022-09-18 19:37:18 -0400

    add nx lib for ui tabs

--- apps/omni-method/src/app/app-routing.module.ts
+++ apps/omni-method/src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
+  {
+    path: 'lib',
+    loadChildren: () =>
+    import('@omni-method-nx/ui').then((m) => m.TabsComponent),
+  }
 ];
 @NgModule({
   imports: [
